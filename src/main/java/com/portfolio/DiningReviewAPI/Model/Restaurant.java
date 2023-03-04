99ff32642bd20b0e735f05d10b6cf5374ab5bd39
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2023-03-04 01:47:40 -0500

    refactored using lombok

--- src/main/java/com/portfolio/DiningReviewAPI/Model/Restaurant.java
+++ src/main/java/com/portfolio/DiningReviewAPI/Model/Restaurant.java
@@ -1,66 +1,26 @@
 package com.portfolio.DiningReviewAPI.Model;
 
 import jakarta.persistence.*; 
+import lombok.Getter;
+import lombok.Setter;
 
-//this is basically a table. "restaurant" table. each field is a column
+//the following annotations generate Setter and Getter methods for each field
+@Setter
+@Getter
+
+//this class basically represents a table called "restaurant". each field is a column
 @Entity
 @Table(name="RESTAURANT")
 public class Restaurant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long restaurantId;
+    private Long id;
     private String name;
     private String address;
     private Double peanutAllergyScore;
     private Double eggAllergyScore;
     private Double dairyAllergyScore;
     private Double overallScore;
-
-    //the following code are setters and getters for each field
-
-    public void setRestaurantId(Long restaurantId){
-        this.restaurantId = restaurantId;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setAddress(String address){
-        this.address = address;
-    }
-    public void setPeanutAllergyScore(Double peanutAllergyScore){
-        this.peanutAllergyScore = peanutAllergyScore;
-    }
-    public void setEggAllergyScore(Double eggAllergyScore){
-        this.eggAllergyScore = eggAllergyScore;
-    }
-    public void setDairyAllergyScore(Double dairyAllergyScore){
-        this.dairyAllergyScore = dairyAllergyScore;
-    }
-    public void setOverallScore(Double overallScore){
-        this.overallScore = overallScore;
-    }
-     
-    public Long getRestaurantId(){
-        return this.restaurantId;
-    }
-    public String getName(){
-        return this.name;
-    }
-    public String getAddress(){
-        return this.address;
-    }
-    public Double getPeanutAllergyScore(){
-        return this.peanutAllergyScore;
-    }
-    public Double getEggAllergyScore(){
-        return this.eggAllergyScore;
-    }
-    public Double getDairyAllergyScore(){
-        return this.dairyAllergyScore;
-    }
-    public Double getOverallScore(){
-        return this.overallScore;
-    }
 
     
 }
