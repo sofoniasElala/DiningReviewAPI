53470a48969b13bcc1174c32defb54e17015dfd7
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2023-03-04 06:55:09 -0500

    AdminReviewAction non-entity class created

---
+++ src/main/java/com/portfolio/DiningReviewAPI/Model/AdminReviewAction.java
@@ -0,0 +1,12 @@
+package com.portfolio.DiningReviewAPI.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminReviewAction {
+
+    private Boolean acceptReview;
+
+}
