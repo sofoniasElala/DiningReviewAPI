a063265b065ab6c0d581c7da4542bdbfb66890d9
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2023-03-03 23:31:56 -0500

    initial spring boot setup

---
+++ src/main/java/com/portfolio/DiningReviewAPI/DiningReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.DiningReviewAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningReviewApiApplication.class, args);
+	}
+
+}
