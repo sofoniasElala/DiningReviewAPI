d523630000800e2253fdafdf67fd30a8b63216d7
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2023-03-04 08:45:18 -0500

    deleted a unnecessary field

--- src/main/java/com/portfolio/DiningReviewAPI/Repository/UserRepository.java
+++ src/main/java/com/portfolio/DiningReviewAPI/Repository/UserRepository.java
@@ -6,6 +6,5 @@ import java.util.Optional;
 
 //CRUD repository for Entity: User
 public interface UserRepository extends CrudRepository<User, Long> {
-    Optional<User> findByUserNameOptional(String userName);
-    User findByUserName(String userName);
+    Optional<User> findByUserNameIgnoreCase(String userName);
 }
