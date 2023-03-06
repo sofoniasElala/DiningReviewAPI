a6f4517bc6cf1be96ef4f6be3ba0c51d8ec4cd6a
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2023-03-06 03:49:46 -0500

    usersController created

---
+++ src/main/java/com/portfolio/DiningReviewAPI/Controller/UsersController.java
@@ -0,0 +1,67 @@
+package com.portfolio.DiningReviewAPI.Controller;
+
+import org.springframework.web.bind.annotation.*;
+import com.portfolio.DiningReviewAPI.Model.*;
+import com.portfolio.DiningReviewAPI.Repository.*;
+import java.util.Optional;
+import org.springframework.web.server.ResponseStatusException;
+
+import org.springframework.util.ObjectUtils;
+
+
+import org.springframework.http.HttpStatus;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    private UserRepository userRepository;
+
+    public UsersController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.FOUND)
+    public User getUser(@RequestParam String userName){
+        Optional<User> userExists = userRepository.findByUserNameIgnoreCase(userName);
+
+        if(!userExists.isPresent()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not found.");
+
+        return userExists.get();
+
+    }
+
+    @PostMapping("/create_user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody User user){
+        Optional<User> userExistsAlready= userRepository.findByUserNameIgnoreCase(user.getUserName());
+
+        if(ObjectUtils.isEmpty(user.getUserName())) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User name is required.");
+        if(userExistsAlready.isPresent()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User name taken.");
+
+        User newUser = userRepository.save(user);
+
+        return newUser;
+    }
+
+    @PutMapping("/update_user_profile")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public User updateUserProfile(@RequestBody User user){
+        User updatedUser;
+        Optional<User> userExists = userRepository.findByUserNameIgnoreCase(user.getUserName());
+        if(!userExists.isPresent()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not found.");
+
+        updatedUser = userExists.get();
+
+        updatedUser.setState(user.getState());
+        updatedUser.setCity(user.getCity());
+        updatedUser.setZip(user.getZip());
+        updatedUser.setDairyAllergy(user.getDairyAllergy());
+        updatedUser.setEggAllergy(user.getEggAllergy());
+        updatedUser.setPeanutAllergy(user.getPeanutAllergy());
+
+        return updatedUser;
+    }
+    
+}
